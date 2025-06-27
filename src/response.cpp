7405e1e3b690858d139a6fc51a82a2bf4c23fb9f
TusharSachdeva29 <sachdevatushar81@gmail.com>
2025-06-27 11:05:29 +0530

    suth frontend

---
+++ src/response.cpp
@@ -0,0 +1,58 @@
+#include "response.hpp"
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <sys/socket.h>
+#include <vector>
+
+std::string generateResponse(int statusCode, const std::string &statusMessage,
+                             const std::string &contentType, const std::string &body,
+                             const std::vector<std::string> &customHeaders)
+{
+    std::string response =
+        "HTTP/1.1 " + std::to_string(statusCode) + " " + statusMessage + "\r\n" +
+        "Content-Type: " + contentType + "\r\n" +
+        "Connection: close\r\n" +
+        "Content-Length: " + std::to_string(body.size()) + "\r\n";
+
+    for (const auto &header : customHeaders) {
+        response += header + "\r\n";
+    }
+
+    response += "\r\n";
+
+    response += body;
+
+    return response;
+}
+
+std::string redirectResponse(int statusCode, const std::string &statusMessage,
+                             const std::string &contentType, const std::string &body,
+                             const std::vector<std::pair<std::string, std::string>> &customHeaders)
+{
+    std::string response =
+        "HTTP/1.1 " + std::to_string(statusCode) + " " + statusMessage + "\r\n" +
+        "Content-Type: " + contentType + "\r\n" +
+        "Connection: close\r\n";
+
+    response += "Content-Length: 0\r\n";
+
+    for (const auto &header : customHeaders) {
+        response += header.first + ": " + header.second + "\r\n";
+    }
+
+    response += "\r\n";
+
+    return response;
+}
+
+
+void sendResponse(int clientSockfd, const std::string &response)
+{
+    if (write(clientSockfd, response.c_str(), response.size()) < 0)
+    {
+        perror("Error writing to socket");
+    }
+    shutdown(clientSockfd, SHUT_RDWR);
+    close(clientSockfd);
+}
---
+++ src/server.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <array>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sstream>
+#include <thread>
+#include <cstring>
+#include <fcntl.h>
+#include <cerrno>
+#include "routes/auth/register.hpp"
+#include "routes/auth/login.hpp"
+#include "routes/user/get-user.hpp"
+#include "routes/user/get-all-users.hpp"
+#include "routes/url/url-shorten.hpp"
+#include "routes/url/redirect.hpp"
+#include "routes/404NotFound.hpp"
+#include "utils/helper.hpp"
+
+class Server
+{
+public:
+    Server(int port) : port_(port) {}
+
+    void start()
+    {
+        int sockfd = createSocket();
+        bindSocket(sockfd);
+        listenOnSocket(sockfd);
+
+        std::cout << "Server started on Port : " << port_ << " (IPv6)" << std::endl;
+
+        while (true)
+        {
+            int clientSockfd = acceptConnection(sockfd);
+            std::thread clientThread(&Server::handleRequest, this, clientSockfd);
+            clientThread.detach();
+        }
+
+        close(sockfd);
+    }
+
+private:
+    int port_;
+
+    int createSocket()
+    {
+        int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
+        if (sockfd < 0)
+        {
+            perror("Error opening socket");
+            exit(1);
+        }
+        return sockfd;
+    }
+
+    void bindSocket(int sockfd)
+    {
+        struct sockaddr_in6 serverAddr = {};
+        serverAddr.sin6_family = AF_INET6;
+        serverAddr.sin6_addr = in6addr_any;
+        serverAddr.sin6_port = htons(port_);
+
+        if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+        {
+            perror("Binding failed");
+            exit(1);
+        }
+    }
+
+    void listenOnSocket(int sockfd)
+    {
+        if (listen(sockfd, 5) < 0)
+        {
+            perror("Error on listen");
+            exit(1);
+        }
+    }
+
+    int acceptConnection(int sockfd)
+    {
+        struct sockaddr_in6 clientAddr = {};
+        socklen_t clientLen = sizeof(clientAddr);
+        int newsockfd = accept(sockfd, (struct sockaddr *)&clientAddr, &clientLen);
+        if (newsockfd < 0)
+        {
+            perror("Error on accept");
+            exit(1);
+        }
+        return newsockfd;
+    }
+
+    void handleRequest(int clientSockfd)
+    {
+        char buffer[4096];
+        memset(buffer, 0, sizeof(buffer));
+
+        int n = recv(clientSockfd, buffer, sizeof(buffer) - 1, 0);
+        if (n < 0)
+        {
+            perror("Error on receiving");
+            close(clientSockfd);
+            return;
+        }
+
+        std::string request(buffer);
+        std::istringstream requestStream(request);
+        std::string method, path, version;
+        std::string refreshToken;
+        requestStream >> method >> path >> version;
+
+        std::string body;
+        std::string line;
+        while (std::getline(requestStream, line))
+        {
+            if(line.substr(0,7) == "vardaan" || !line.substr(0,7).compare("vardaan")){
+                body = line.substr(8);
+            }
+            if(line.find("Cookie")!=std::string::npos){
+                refreshToken = extractRefreshToken(line);
+            }
+        }
+
+        if (path == "/register")
+        {
+            handleRegister(clientSockfd, method, path, body);
+        }
+        else if (path == "/login")
+        {
+            handleLogin(clientSockfd, method, path, body);
+        }
+        else if (path == "/get-user")
+        {
+            getUserByEmail(clientSockfd, method, path, body);
+        }
+        else if (path == "/get-all-users")
+        {
+            getAllUsers(clientSockfd, method, path, body,refreshToken);
+        }
+        else if (path == "/url-shortener")
+        {
+            shortenURL(clientSockfd, method, path, body,refreshToken);
+        }
+        else if(path.find("/vardaan.ly")!=std::string::npos){
+            redirect(clientSockfd, method, path);
+        }
+        else
+        {
+            handle404NotFound(clientSockfd);
+        }
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    int port = (argc < 2) ? 8080 : std::stoi(argv[1]);
+    Server server(port);
+    server.start();
+    return 0;
+}
